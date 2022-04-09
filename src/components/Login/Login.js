e51c1b7c783af273661d2fdc1e463ece45942a94
Rasujon3 <rasujon3@gmail.com>
2022-04-09 11:00:16 +0600

    58-5 Explore and Install React Firebase Hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,11 @@
 import React from "react";
-import useFirebase from "../../hooks/useFirebase";
 
 const Login = () => {
-  const { signInWithGoogle } = useFirebase();
   return (
     <div>
       <h3>Please login</h3>
       <div style={{ margin: "20px" }}>
-        <button onClick={signInWithGoogle}>Google Sign In</button>
+        <button onClick={""}>Google Sign In</button>
       </div>
       <form>
         <input type="email" placeholder="Your Email" /> <br />
