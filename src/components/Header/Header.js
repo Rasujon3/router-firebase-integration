f5eb0c15afd45a66e9b5e2c377d6d7a280e73396
Rasujon3 <rasujon3@gmail.com>
2022-04-09 11:20:25 +0600

    58-6 Use React Firebase Hook to manage user authentication

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
-import useFirebase from "../../hooks/useFirebase";
 import "./Header.css";
+import { getAuth, signOut } from "firebase/auth";
+import app from "./../../firebase.init";
+
+const auth = getAuth(app);
 
 const Header = () => {
-  const { user, handleSignOut } = useFirebase();
-  console.log(user);
+  const [user] = useAuthState(auth);
   return (
     <div className="header">
       <nav>
@@ -15,7 +18,7 @@ const Header = () => {
         <Link to="/register">Register</Link>
         <span>{user?.displayName && user.displayName}</span>
         {user?.uid ? (
-          <button onClick={handleSignOut}>Sign Out</button>
+          <button onClick={() => signOut(auth)}>Sign Out</button>
         ) : (
           <Link to="/login">Login</Link>
         )}
